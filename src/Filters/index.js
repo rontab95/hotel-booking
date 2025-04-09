0915cfa55f3ab34bb0d11c4e5ef9c7388170e4df
rontab95 <rontab95@yahoo.com>
2025-04-09 10:12:42 -0700

    fixing

--- src/Filters/index.js
+++ src/Filters/index.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { filterOptions } from "../data.js";
 
 const FiltersList = (props) => {
+  const [selectedValue, setSelectedValue] = useState("");
+
   const handleChange = (event) => {
     const selectedKey = event.target.value;
-    props.toggleFilter(selectedKey);
+    setSelectedValue(""); // Reset dropdown after selection
+    if (selectedKey) {
+      props.toggleFilter(selectedKey);
+    }
   };
 
   return (
     <div className="filters-dropdown">
       <label htmlFor="filter-select"><strong>Filter by:</strong></label>
-      <select id="filter-select" onChange={handleChange} value="">
-        <option value="" disabled>Choose a filter</option>
+      <select
+        id="filter-select"
+        value={selectedValue}
+        onChange={handleChange}
+      >
+        <option value="" disabled>
+          Choose a filter
+        </option>
         {filterOptions.map((filter) => (
           <option key={filter.key} value={filter.key}>
             {filter.display}
